1377961ed6b2a6a6d6e69be1a5db57a2bd5f6f60
unknown <njengagichuhi211@gmail.com>
2023-09-29 02:13:29 +0300

    reaction-timer

---
+++ src/components/Block.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+      timer: true,
+      reactionTime: 0,
+    };
+  },
+  mounted() {
+    console.log("i am good at this");
+    setTimeout(() => {
+      this.showBlock = true;
+      this.starTimer();
+    }, this.delay);
+  },
+  methods: {
+    starTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      });
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      console.log(this.reactionTime);
+      this.$emit("end", this.reactionTime);
+    },
+    
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background: #afaf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Reaction Time {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "thumbs up";
+    } else if (this.score < 400) {
+      this.rank = "Rapid reflexes";
+    } else {
+      This.rank = "snail pace";
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
